3d6bda1fb0a0930a947ec01170c52dd42f9fd070
Joao Ferreira <joao.mdvferreira@gmail.com>
2020-12-09 13:08:45 +0000

    adding quotes file

--- bot.js
+++ bot.js
@@ -1,73 +1,38 @@
 import dotenv from 'dotenv';
 import { Client, MessageEmbed } from 'discord.js';
+import { zagaQuotes, broaQuotes, kombatQuotes } from './quotes.js';
 dotenv.config();
 const client = new Client();
 
 const BOT_PREFIX = "!";
-const BOT_COMMAND = "quote";
-
-const ADMIN_COMMAND = "!!admin"
-
-const zagaQuotes = [
-    "Nasci sem balas",
-    "Tava de costas",
-    "Bellaaaa",
-    "Só dizem quando faço mal..",
-    "Ajudem!",
-    "Melll"
-]
-
-const broaQuotes = [
-    "Aproveita isto!",
-    "LIASSARAAA"
-]
-
-const kombatQuotes = [
-    "Faker",
-    "Akali one trick",
-    "CR7 do LoL"
-]
+const QUOTE_BOT_COMMAND = "quote";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// client.on('message', msg => {
-//     if (msg.content === `${BOT_PREFIX}${BOT_COMMAND} zaga`) {
-//         const message = zagaQuotes[Math.floor(Math.random() * zagaQuotes.length)];
-//         const embed = new MessageEmbed()
-//             .setTitle('Zaga Quote')
-//             .setColor(0xff0000)
-//             .setDescription(message);
-//         msg.channel.send(embed);
-//     } else if (msg.content === `${BOT_PREFIX}${BOT_COMMAND} broa`) {
-//         const message = broaQuotes[Math.floor(Math.random() * broaQuotes.length)];
-//         const embed = new MessageEmbed()
-//             .setTitle('Broa Quote')
-//             .setColor(0xff0000)
-//             .setDescription(message);
-//         msg.channel.send(embed);
-//     } else if (msg.content === `${BOT_PREFIX}${BOT_COMMAND} 4kombat`) {
-//         const message = kombatQuotes[Math.floor(Math.random() * kombatQuotes.length)];
-//         const embed = new MessageEmbed()
-//             .setTitle('4kombat Quote')
-//             .setColor(0xff0000)
-//             .setDescription(message);
-//         msg.channel.send(embed);
-//     }
-// });
-
 client.on('message', msg => {
-    if (msg.content === `${ADMIN_COMMAND} id`) {
-        msg.channel.send(msg.author.id);
-    } else if (msg.content === `${ADMIN_COMMAND} tag`) {
-        msg.channel.send(msg.author.tag);
-    } else if (msg.content === `${ADMIN_COMMAND} users`) {
-        const users = [];
-        msg.guild.channels.forEach(user => {
-            users.push(user);
-        })
-        msg.channel.send(users);
+    if (msg.content === `${BOT_PREFIX}${QUOTE_BOT_COMMAND} zaga`) {
+        const message = zagaQuotes[Math.floor(Math.random() * zagaQuotes.length)];
+        const embed = new MessageEmbed()
+            .setTitle('Zaga Quote')
+            .setColor(0xff0000)
+            .setDescription(message);
+        msg.channel.send(embed);
+    } else if (msg.content === `${BOT_PREFIX}${QUOTE_BOT_COMMAND} broa`) {
+        const message = broaQuotes[Math.floor(Math.random() * broaQuotes.length)];
+        const embed = new MessageEmbed()
+            .setTitle('Broa Quote')
+            .setColor(0xff0000)
+            .setDescription(message);
+        msg.channel.send(embed);
+    } else if (msg.content === `${BOT_PREFIX}${QUOTE_BOT_COMMAND} 4kombat`) {
+        const message = kombatQuotes[Math.floor(Math.random() * kombatQuotes.length)];
+        const embed = new MessageEmbed()
+            .setTitle('4kombat Quote')
+            .setColor(0xff0000)
+            .setDescription(message);
+        msg.channel.send(embed);
     }
 });
 
---
+++ quotes.js
@@ -0,0 +1,19 @@
+export const zagaQuotes = [
+    "Nasci sem balas",
+    "Tava de costas",
+    "Bellaaaa",
+    "Só dizem quando faço mal..",
+    "Ajudem!",
+    "Melll"
+]
+
+export const broaQuotes = [
+    "Aproveita isto!",
+    "LIASSARAAA"
+]
+
+export const kombatQuotes = [
+    "Faker",
+    "Akali one trick",
+    "CR7 do LoL"
+]
